b6ee3b816cf49d25c5b41484f6f30b882c736b38
Roman <ramzos@rambler.ru>
2022-02-26 13:16:09 +0300

    DzLessons1.10 выполнено

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -21,18 +21,18 @@ public class Main {
         Author tomDeMarco = new Author("Tom ", "DeMarco");
         Author tomDeMarco2 = new Author("Tom ", "DeMarco");
         Author levTolstoy = new Author("Лев ", "Толстой");
-        System.out.println("tomDeMarco.equals(tomDeMarco2) = " + tomDeMarco.equals(tomDeMarco2));
-        System.out.println("tomDeMarco.equals(levTolstoy) = " + tomDeMarco.equals(levTolstoy));
-        System.out.println("tomDeMarco2.equals(levTolstoy) = " + tomDeMarco2.equals(levTolstoy) + "\n");
+        System.out.println("Author copies equals: " + tomDeMarco.equals(tomDeMarco2));
+        System.out.println("Author copies equals by hashCode: " + (tomDeMarco.hashCode() == tomDeMarco2.hashCode()));
+        System.out.println("Author copies equals: " + tomDeMarco2.equals(levTolstoy) + "\n");
 
 
         System.out.println("Сравнение для Book: \n");
         Book tomDeMarcoBook = new Book("Deadline", new Author("Tom", "DeMarco"), 1997);
         Book levTolstoyBook = new Book("Война и мир", new Author("Лев", "Тольстой"), 1865);
         Book levTolstoyBook2 = new Book("Война и мир", new Author("Лев", "Тольстой"), 1869);
-        System.out.println("tomDeMarcoBook.equals(levTolstoyBook) = " + tomDeMarcoBook.equals(levTolstoyBook));
-        System.out.println("tomDeMarco.equals(levTolstoyBook2) = " + tomDeMarcoBook.equals(levTolstoyBook2));
-        System.out.println("levTolstoyBook.equals(levTolstoyBook2) = " + levTolstoyBook.equals(levTolstoyBook2));
+        System.out.println("Book copies equals: " + tomDeMarcoBook.equals(levTolstoyBook));
+        System.out.println("Book copies equals: " + tomDeMarcoBook.equals(levTolstoyBook2));
+        System.out.println("Book copies equals by hashCode: " + (levTolstoyBook.hashCode() == levTolstoyBook2.hashCode()));
 
     }
 }
